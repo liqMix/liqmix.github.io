d875359417abd8d68431c0abbf89d0b84eee0814
Brady Young <ybrady@pdx.edu>
2019-07-12 19:33:38 -0700

    Updates

---
+++ precache-manifest.c8cabb3136ff5a4294d1b1ed261f4cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6b5c2e2d7b898058588e004d80a973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "352cfa6a1d49a36ba3ea",
+    "url": "/static/css/main.1c808f65.chunk.css"
+  },
+  {
+    "revision": "209e164afb363e135554",
+    "url": "/static/js/2.5a553c59.chunk.js"
+  },
+  {
+    "revision": "352cfa6a1d49a36ba3ea",
+    "url": "/static/js/main.2ef8e894.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9686ce029e9beaebee75f0def6d61b98",
+    "url": "/static/media/bg.9686ce02.jpg"
+  }
+]);
